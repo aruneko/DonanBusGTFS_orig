c6b5c3a6779bb8c72c82c7a6f801cd7b71e6627f
Yuki <aruneko99@gmail.com>
2017-12-11 16:38:14 +0900

    小数の精度で59秒になってしまっていた問題を修正

--- bin/CreateStopTimes.py
+++ bin/CreateStopTimes.py
@@ -8,13 +8,14 @@ HOLY_FILE_NAME = '../raw/normalized_holiday_20170401.xls'
 HEADER = 'trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled,timepoint'
 
 
-def create_stop_times(sheets, service_id):
+def create_stop_times(sheets, book, service_id):
     for sheet in sheets:
         for i in range(1, sheet.nrows):
             row = sheet.row_values(i)
             trip_id = sheet.name
-            raw_time = int(row[3] * 24 * 3600)
-            arrival_time = time(raw_time//3600, (raw_time % 3600)//60, raw_time % 60)
+            # raw_time = int(row[3] * 24 * 3600)
+            # arrival_time = time(raw_time//3600, (raw_time % 3600)//60, raw_time % 60)
+            arrival_time = xlrd.xldate.xldate_as_datetime(row[3], book.datemode).strftime('%H:%M:%S')
             stop_id = f'{row[1]}_{row[2]}'
             stop_seq = i
             pick_type = 3 if i is not sheet.nrows - 1 else 1
@@ -24,11 +25,14 @@ def create_stop_times(sheets, service_id):
 
 
 def main():
-    week_sheets_file = xlrd.open_workbook(WEEK_FILE_NAME).sheets()
-    holy_sheets_file = xlrd.open_workbook(HOLY_FILE_NAME).sheets()
+    week_workbook = xlrd.open_workbook(WEEK_FILE_NAME)
+    holy_workbook = xlrd.open_workbook(HOLY_FILE_NAME)
 
-    week_stop_times = create_stop_times(week_sheets_file, 'weekday')
-    holy_stop_times = create_stop_times(holy_sheets_file, 'holiday')
+    week_sheets_file = week_workbook.sheets()
+    holy_sheets_file = holy_workbook.sheets()
+
+    week_stop_times = create_stop_times(week_sheets_file, week_workbook, 'weekday')
+    holy_stop_times = create_stop_times(holy_sheets_file, holy_workbook, 'holiday')
 
     print(HEADER)
 
