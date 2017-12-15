54ad22de8b2c38dbd8d09d767bdf3bc995274e16
Aruneko <aruneko99@gmail.com>
2017-12-15 14:56:37 +0900

    holidayからweekendに変更

--- bin/CreateTrips.py
+++ bin/CreateTrips.py
@@ -2,14 +2,10 @@
 
 import xlrd
 from itertools import chain
+from util import WEEKDAY_FILE_NAME, WEEKEND_FILE_NAME, extract_valid_sheets
 
-WEEK_FILE_NAME = '../raw/timetable_weekday_20170401.xls'
-HOLY_FILE_NAME = '../raw/timetable_holyday_20170401.xls'
-HEADER = 'route_id,service_id,trip_id,trip_headsign,trip_short_name,direction_id,block_id,shape_id,wheelchair_accessible,bikes_allowed,jp_trip_desc,jp_trip_desc_symbol,jp_office_id'
-
-
-def extract_valid_sheets(sheets_file):
-    return [s for s in sheets_file if int(s.name) < 140000 and s.ncols != 2]
+HEADER = 'route_id,service_id,trip_id,trip_headsign,trip_short_name,direction_id,block_id,' \
+         'shape_id,wheelchair_accessible,bikes_allowed,jp_trip_desc,jp_trip_desc_symbol,jp_office_id'
 
 
 def get_dir(route_id):
@@ -56,24 +52,24 @@ def create_trips(sheets, service_id, shapes):
 
 
 def main():
-    week_sheets_file = xlrd.open_workbook(WEEK_FILE_NAME).sheets()
-    holy_sheets_file = xlrd.open_workbook(HOLY_FILE_NAME).sheets()
+    weekday_sheets_file = xlrd.open_workbook(WEEKDAY_FILE_NAME).sheets()
+    weekend_sheets_file = xlrd.open_workbook(WEEKEND_FILE_NAME).sheets()
 
-    raw_week_sheets = extract_valid_sheets(week_sheets_file)
-    raw_holy_sheets = extract_valid_sheets(holy_sheets_file)
+    raw_weekday_sheets = extract_valid_sheets(weekday_sheets_file)
+    raw_weekend_sheets = extract_valid_sheets(weekend_sheets_file)
 
     shape_file = open('../raw/trip_shape.csv', 'r').readlines()
     shapes = [s[:-1].split(',') for s in shape_file]
 
-    week_trips = create_trips(raw_week_sheets, 'weekday', shapes)
-    holy_trips = create_trips(raw_holy_sheets, 'holiday', shapes)
+    weekday_trips = create_trips(raw_weekday_sheets, 'weekday', shapes)
+    weekend_trips = create_trips(raw_weekend_sheets, 'weekend', shapes)
 
     print(HEADER)
 
-    for t in week_trips:
+    for t in weekday_trips:
         print(t)
 
-    for t in holy_trips:
+    for t in weekend_trips:
         print(t)
 
 
