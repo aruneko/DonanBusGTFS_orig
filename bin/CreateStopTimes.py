883ce9fa85006ef858c415b215c52f0564864310
Aruneko <aruneko99@gmail.com>
2017-12-15 14:52:14 +0900

    holidayからweekendに変更

--- bin/CreateStopTimes.py
+++ bin/CreateStopTimes.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
 import xlrd
-from datetime import time
 
-WEEK_FILE_NAME = '../raw/normalized_weekday_20170401.xls'
-HOLY_FILE_NAME = '../raw/normalized_holiday_20170401.xls'
-HEADER = 'trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled,timepoint'
+WEEKDAY_FILE_NAME = '../raw/normalized_weekday_20170401.xls'
+WEEKEND_FILE_NAME = '../raw/normalized_weekend_20170401.xls'
+HEADER = 'trip_id,arrival_time,departure_time,stop_id,stop_sequence,' \
+         'stop_headsign,pickup_type,drop_off_type,shape_dist_traveled,timepoint'
 
 
-def create_stop_times(sheets, book, service_id):
+def create_stop_times(sheets, book):
     for sheet in sheets:
         for i in range(1, sheet.nrows):
             row = sheet.row_values(i)
             trip_id = sheet.name
-            # raw_time = int(row[3] * 24 * 3600)
-            # arrival_time = time(raw_time//3600, (raw_time % 3600)//60, raw_time % 60)
             arrival_time = xlrd.xldate.xldate_as_datetime(row[3], book.datemode).strftime('%H:%M:%S')
             stop_id = f'{row[1]}_{row[2]}'
             stop_seq = i
@@ -25,21 +23,21 @@ def create_stop_times(sheets, book, service_id):
 
 
 def main():
-    week_workbook = xlrd.open_workbook(WEEK_FILE_NAME)
-    holy_workbook = xlrd.open_workbook(HOLY_FILE_NAME)
+    weekday_workbook = xlrd.open_workbook(WEEKDAY_FILE_NAME)
+    weekend_workbook = xlrd.open_workbook(WEEKEND_FILE_NAME)
 
-    week_sheets_file = week_workbook.sheets()
-    holy_sheets_file = holy_workbook.sheets()
+    weekday_sheets_file = weekday_workbook.sheets()
+    weekend_sheets_file = weekend_workbook.sheets()
 
-    week_stop_times = create_stop_times(week_sheets_file, week_workbook, 'weekday')
-    holy_stop_times = create_stop_times(holy_sheets_file, holy_workbook, 'holiday')
+    weekday_stop_times = create_stop_times(weekday_sheets_file, weekday_workbook)
+    weekend_stop_times = create_stop_times(weekend_sheets_file, weekend_workbook)
 
     print(HEADER)
 
-    for s in week_stop_times:
+    for s in weekday_stop_times:
         print(s)
 
-    for s in holy_stop_times:
+    for s in weekend_stop_times:
         print(s)
 
 
