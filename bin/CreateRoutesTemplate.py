d5f821ccef87779f057dc258153b47a78a4f7c99
Aruneko <aruneko99@gmail.com>
2017-12-15 14:26:02 +0900

    holidayをweekendに変更

--- bin/CreateRoutesTemplate.py
+++ bin/CreateRoutesTemplate.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
 import xlrd
+from util import WEEKDAY_FILE_NAME, WEEKEND_FILE_NAME, extract_valid_sheets
 
-WEEK_FILE_NAME = '../raw/timetable_weekday_20170401.xls'
-HOLY_FILE_NAME = '../raw/timetable_holyday_20170401.xls'
 HEADER = 'route_id,agency_id,route_short_name,route_long_name,route_desc,route_type,route_url,route_color,route_text_color,jp_parent_route_id'
-
-
-def extract_valid_sheets(sheets_file):
-    return [s for s in sheets_file if int(s.name) < 140000 and s.ncols != 2]
 
 
 def extract_sheet_names(sheets):
@@ -24,16 +19,16 @@ def create_routes(names):
 
 
 def main():
-    week_sheets_file = xlrd.open_workbook(WEEK_FILE_NAME).sheets()
-    holy_sheets_file = xlrd.open_workbook(HOLY_FILE_NAME).sheets()
+    weekday_sheets_file = xlrd.open_workbook(WEEKDAY_FILE_NAME).sheets()
+    weekend_sheets_file = xlrd.open_workbook(WEEKEND_FILE_NAME).sheets()
 
-    raw_week_sheets = extract_valid_sheets(week_sheets_file)
-    raw_holy_sheets = extract_valid_sheets(holy_sheets_file)
+    raw_weekday_sheets = extract_valid_sheets(weekday_sheets_file)
+    raw_weekend_sheets = extract_valid_sheets(weekend_sheets_file)
 
-    week_sheet_names = extract_sheet_names(raw_week_sheets)
-    holy_sheet_names = extract_sheet_names(raw_holy_sheets)
+    weekday_sheet_names = extract_sheet_names(raw_weekday_sheets)
+    weekend_sheet_names = extract_sheet_names(raw_weekend_sheets)
 
-    names = week_sheet_names + holy_sheet_names
+    names = weekday_sheet_names + weekend_sheet_names
     without_duplicates = sorted(set(names))
     routes = create_routes(without_duplicates)
 
