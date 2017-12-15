da7f239b7b052503e494ee6f333c2cb46280962a
Aruneko <aruneko99@gmail.com>
2017-12-15 14:21:28 +0900

    flake8の警告に従ってコードをリファクタリング

--- bin/CreateShapes.py
+++ bin/CreateShapes.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-import json
 import math
 import requests
 
+
 def create_req():
-    #entry_url = 'http://overpass.osm.rambler.ru/cgi/interpreter?data='
     entry_url = 'http://overpass-api.de/api/interpreter?data='
     query = '[out:json];area["name"~"室蘭市|登別市"];rel(area)[route=bus];out geom;'
     return requests.get(entry_url + query).json()['elements']
+
 
 def extract_position(way):
     if way['role'] == 'forward':
@@ -16,25 +16,30 @@ def extract_position(way):
     else:
         return list(reversed(way['geometry']))
 
+
 def concat_position(src, res):
     if len(src) == 1:
         return res + src[0]
     else:
         return concat_position(src[1:], res + src[0][:-1])
 
+
 def create_shape(rel):
     rel_id = rel['id']
     ways = [w for w in rel['members'] if w['role'] != 'stop']
     geoms = [extract_position(g) for g in ways]
-    return (rel_id, concat_position(geoms, []))
+    return rel_id, concat_position(geoms, [])
+
 
 def lat_to_km(lat):
     pole_r = 6356.752314
     return 2 * math.pi * pole_r / 360 * lat
 
+
 def lon_to_km(lat, lon):
     eq_r = 6378.137
-    return  2 * math.pi * eq_r * math.cos(math.radians(lat)) / 360 * lon
+    return 2 * math.pi * eq_r * math.cos(math.radians(lat)) / 360 * lon
+
 
 def calc_dist(lat_prev, lon_prev, lat, lon):
     lat_diff = lat_to_km(abs(lat - lat_prev))
@@ -42,13 +47,18 @@ def calc_dist(lat_prev, lon_prev, lat, lon):
     lon_diff = lon_to_km(lat_ave, abs(lon_prev - lon))
     return math.sqrt(pow(lat_diff, 2) + pow(lon_diff, 2))
 
-print('shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence,shape_dist_traveled')
-shapes = [create_shape(r) for r in create_req()]
-for (s_id, shape) in shapes:
-    diff = 0
-    lat_prev, lon_prev = (shape[0]['lat'], shape[0]['lon'])
-    for (i, s) in enumerate(shape):
-        diff += calc_dist(lat_prev, lon_prev, s['lat'], s['lon'])
-        lat_prev, lon_prev = (s['lat'], s['lon'])
-        print(f"{s_id},{s['lat']},{s['lon']},{i},{diff}")
 
+def main():
+    print('shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence,shape_dist_traveled')
+    shapes = [create_shape(r) for r in create_req()]
+    for (s_id, shape) in shapes:
+        diff = 0
+        lat_prev, lon_prev = (shape[0]['lat'], shape[0]['lon'])
+        for (i, s) in enumerate(shape):
+            diff += calc_dist(lat_prev, lon_prev, s['lat'], s['lon'])
+            lat_prev, lon_prev = (s['lat'], s['lon'])
+            print(f"{s_id},{s['lat']},{s['lon']},{i},{diff}")
+
+
+if __name__ == '__main__':
+    main()
